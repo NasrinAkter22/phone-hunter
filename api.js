889310ca414e0f9335007c82353e53000552dfbf
NasrinAkter22 <258nasrinakter@gmail.com>
2022-03-01 15:57:20 +0600

    mobile brand

--- api.js
+++ api.js
@@ -1,44 +1,44 @@
 const error=document.getElementById('error')
 
-const loadModule =()=>{
-const input=document.getElementById('inputFild')
-const inputText=input.value
+document.getElementById('loadModule').addEventListener('click',()=>{
 
-if (input.value==''){
-    displayDiv.innerHTML=''
-    error.innerText="please enter valid name"
-    // input.valu=''
-}
-else{
-    error.innerHTML=''
-    displayDiv.innerHTML=''
+    const input=document.getElementById('inputFild')
+    const inputText=input.value;
+    
+    if (input.value==''){
+        displayDiv.innerHTML=''
+        error.innerText="please enter valid name"
+        // input.valu=''
+    }
+    else{
+        const url=`https://openapi.programming-hero.com/api/phones?search=${inputText}`
+        fetch(url)
+        .then(response => response.json())
+        .then(data => displayMobile(data.data.slice(0, 20))) 
+    }
 
-    const url=`https://openapi.programming-hero.com/api/phones?search=${inputText}`
-    console.log(url)
-    fetch(url)
-    .then(response => response.json())
-    .then(data => displayMobile(data.data)) 
-}
-}
+
+})
+
+
+
 const displayMobile=(mData)=>{
-    // console.log(mData);
-    const dataSize=mData.slice(0,20)
+  
     const displayDiv=document.getElementById('mainDiv')
-    dataSize.forEach(mobile => {
+        displayDiv.textContent=''
+    mData.forEach(mobile => {
         console.log(mobile);
-        const div=document.createComment('div')
+        const div=document.createElement('div')
        
-        div.innerHTML=`<div class="card p-5">
+        div.innerHTML=`<div class="card p-2">
         <img src="${mobile.image}"class="card-img-top"alt="...">
          <div class="card-body">
-          <h5 class="class-title"><span class="text-info">Brand:</span>${mobile.brand}</h5>     
-          <h5 class="class-title"><span class="text-info">name:</span>${mobile.brand}</h5> 
-          <button onclick="loadModule()"class="btn bg-primary text-white mt-3">Mobile details</button>
-              
+         <h5 class="class-title"><span class="text-info">name:</span> ${mobile.phone_name}</h5> 
+          <h5 class="class-title"><span class="text-info">Brand:</span> ${mobile.brand}</h5>     
+          <button onclick="loadModule()"class="btn bg-primary text-white mt-3">Mobile details</button>    
 
         </div>
         </div>
-        
         
         `
 
